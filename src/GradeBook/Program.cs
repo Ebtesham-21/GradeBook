f97a50c60edfe376c39f4173211a05e1ce599754
unknown <ebteshamhossain98@gmail.com>
2021-10-13 00:24:40 +0600

    first commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var numbers = new[] { 12.7, 10.3, 6.11 };
+
+
+            var results = numbers[0];
+            results = results + numbers[1];
+            results = results + numbers[2];
+            Console.WriteLine(results);
+
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+
+        }
+    }
+}
